68662d541267024d0d0e4c7383ba3c2ca138c262
zsbalogh <baloghzsof@gmail.com>
2013-04-04 11:23:20 +0200

    Handle fetcher exceptions in omniauth callback controller.

--- vendor/engines/omniauth-fluenta/app/controllers/omniauth_fluenta/omniauth_callbacks_controller.rb
+++ vendor/engines/omniauth-fluenta/app/controllers/omniauth_fluenta/omniauth_callbacks_controller.rb
@@ -11,8 +11,13 @@ module OmniauthFluenta
     end
 
     def fluenta
-      sign_in_and_redirect OmniauthFluenta::Fetcher.user(request.env['omniauth.auth'].to_hash), :event => :authentication
-      set_flash_message(:notice, :success, :kind => "Fluenta") if is_navigational_format?
+      begin
+        sign_in_and_redirect OmniauthFluenta::Fetcher.user(request.env['omniauth.auth'].to_hash), :event => :authentication
+        set_flash_message(:notice, :success, :kind => "Fluenta") if is_navigational_format?
+      rescue StandardError => e
+        logger.debug "\nException occured while trying to sign in via fluenta :\n\t#{e.inspect}\n#{e.backtrace}\n"
+        redirect_to request.env['omniauth.origin'], :alert => _("Internal error occured while trying to sign in through Fluenta...")
+      end
     end
 
   end
--- vendor/engines/omniauth-fluenta/config/initializers/omniauth.rb
+++ vendor/engines/omniauth-fluenta/config/initializers/omniauth.rb
@@ -6,10 +6,6 @@ module OmniAuth
   end
 end
 
-ENV['FLUENTA_KEY']    = '2884cee22cdc089ecbe61518195fa59dd15b4917003da7adf7678acf1c647932'
-ENV['FLUENTA_SECRET'] = '44b7fe4dab69abb82caac717d392ea4915e16f88025f929f99f25f2ff0c81809'
-ENV['FLUENTA_SITE']   = 'http://localhost:3001'
-
 Devise.setup do |config|
   config.omniauth :fluenta, ENV['FLUENTA_KEY'], ENV['FLUENTA_SECRET'], client_options: {site: ENV['FLUENTA_SITE']}
 end
