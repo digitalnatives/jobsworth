3f5179e1d89ac09c01938da26a804d7a69df2178
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2013-04-09 17:44:41 +0200

    Do not depend on unmerged feature

--- db/migrate/20130405120154_create_superadmin_user.rb
+++ db/migrate/20130405120154_create_superadmin_user.rb
@@ -1,21 +1,19 @@
 class CreateSuperadminUser < ActiveRecord::Migration
   def up
-    c = Company.create  fluenta_id: 0,
-                        name: 'Administration',
-                        subdomain: 'administration'
-        User.create     company: c,
-                        fluenta_id: 0,
-                        username: 'admin',
-                        name: 'Administrator',
-                        email: 'superadmin@electool.com',
-                        password: '123456',
-                        password_confirmation: '123456',
-                        superadmin: true
+    c = Company.create name: 'Administration',
+                       subdomain: 'administration'
+        User.create    company: c,
+                       username: 'admin',
+                       name: 'Administrator',
+                       email: 'superadmin@getjobsworth.org',
+                       password: '123456',
+                       password_confirmation: '123456',
+                       superadmin: true
   end
 
   def down
-    Company.find_all_by_fluenta_id(0).each &:destroy
-    User.find_all_by_fluenta_id(0).each &:destroy
+    Company.destroy_all subdomain: 'administration'
+    User.destroy_all super_admin: true
   end
 
 end
