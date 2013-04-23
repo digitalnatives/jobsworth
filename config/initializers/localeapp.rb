1faf2ea4c41953d3a731b9e43a7dd276d58597a4
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2013-04-23 16:12:40 +0200

    Localeapp initializer has a fallback

--- config/initializers/localeapp.rb
+++ config/initializers/localeapp.rb
@@ -1,12 +1,14 @@
 if defined?(Localeapp)
   require 'localeapp/rails'
 
-  if Setting.localeapp!.api_key.present?
-    Localeapp.configure do |config|
+  Localeapp.configure do |config|
+    if defined?(Setting) && Setting.localeapp!.api_key.present?
       config.api_key = Setting.localeapp.api_key
       config.sending_environments   = Setting.localeapp.sending_environments.to_a
       config.polling_environments   = Setting.localeapp.polling_environments.to_a
       config.reloading_environments = Setting.localeapp.reloading_environments.to_a
+    else
+      config.api_key = ENV['LOCALEAPP_TOKEN']
     end
   end
 end
