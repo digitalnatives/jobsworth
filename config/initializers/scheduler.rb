2df792c2790ff06b42647f1a75f3acad862a28a5
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2013-04-25 10:08:04 +0200

    Safely schedule with rufus

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -1,25 +1,39 @@
 require 'rufus/scheduler'
 
+def safely_schedule
+  unless ActiveRecord::Base.connected?
+    ActiveRecord::Base.connection.verify!(0)
+  end
+
+  Rails.logger.tagged "SCHEDULER" do
+    yield
+  end
+rescue => e
+  status e.inspect
+ensure
+  ActiveRecord::Base.connection_pool.release_connection
+end
+
 scheduler = Rufus::Scheduler.start_new
 
 # Every morning at 6:17am
 scheduler.cron '17 6 * * *' do
-  Rails.logger.tagged "SCHEDULER" do
+  safely_schedule do
     Rails.logger.info "Expire hide_until tasks"
     TaskRecord.expire_hide_until
   end
 end
 
 # Schedule tasks every 10 minutes
-scheduler.cron '*/10 * * * *' do
-  Rails.logger.tagged "SCHEDULER" do
+scheduler.every '10m' do
+  safely_schedule do
     User.schedule_tasks
   end
 end
 
 # Every morning at 6:43am
 scheduler.cron '43 6 * * *' do
-  Rails.logger.tagged "SCHEDULER" do
+  safely_schedule do
     Rails.logger.info "Recalculating score values for all the tasks"
     TaskRecord.calculate_score
   end
