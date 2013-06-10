55033eb327905700a03bf3d390f3ea437568bff4
Gergő Sulymosi <gergo.sulymosi@gmail.com>
2013-06-10 14:48:00 +0200

    Prefix the fluenta engine's devise paths

--- vendor/engines/omniauth-fluenta/config/routes.rb
+++ vendor/engines/omniauth-fluenta/config/routes.rb
@@ -1,8 +1,9 @@
 OmniauthFluenta::Engine.routes.draw do
 
+  prefix = ActionController::Base.config.relative_url_root.to_s.sub('/', '')
   devise_scope :user do
-    get '/users/auth/fluenta', to: 'omniauth_callbacks#passthru'
-    get '/users/auth/fluenta/callback', to: 'omniauth_callbacks#fluenta'
+    get "#{prefix}/users/auth/fluenta",          to: 'omniauth_callbacks#passthru', as: :users_auth_fluenta
+    get "#{prefix}/users/auth/fluenta/callback", to: 'omniauth_callbacks#fluenta',  as: :users_auth_fluenta_callback
   end
 
 end
